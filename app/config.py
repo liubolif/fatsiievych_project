7913873f92a1b0d6452caa17418c7a7b21e5889e
liubolif <lyubomyrif@gmail.com>
2021-05-27 21:30:53 +0300

    make it better

--- app/config.py
+++ app/config.py
@@ -5,5 +5,4 @@ class Config:
     SECRET_KEY = os.urandom(24)
     # SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
-    # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(os.path.abspath(os.path.dirname(__file__)),'site.db')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')#'sqlite:///site.db'

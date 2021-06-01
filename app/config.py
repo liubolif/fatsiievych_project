b53d220be062f4b9a42acc789320b87b07a2eadc
liubolif <lyubomyrif@gmail.com>
2021-06-01 14:20:12 +0300

    “lab14_33”

--- app/config.py
+++ app/config.py
@@ -5,4 +5,4 @@ class Config:
     SECRET_KEY = os.urandom(24)
     # SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') #or 'sqlite:///site.db' ##
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///site.db' ##

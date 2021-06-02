77f352895118c19606c2497e8327308ba13b5273
liubolif <lyubomyrif@gmail.com>
2021-06-02 15:11:17 +0300

    “lab14_43”

--- commands.py
+++ commands.py
@@ -1,42 +1,42 @@
 import click
 from flask.cli import with_appcontext
-from app import db
+
+from app import db, app
 from app.profile.models import User
-from app.task.models import Task, Category, Employee
+
+
+@app.cli.command()
+def initdb():
+    """Initialize the database."""
+    click.echo('Init the db')
+
 
 @click.group()
 def cli():
     pass
 
-@click.command(name="create_tables")
+
+@click.command(name='create_tables')
 @with_appcontext
 def create_tables():
     db.create_all()
     click.echo("tables created!!!")
+    # print("create_all was maked")
 
 
 @click.command(name='create_admin')
 @with_appcontext
 def create_admin():
-    username = "test_admin"
-    email = "test_admin@gmail.com"
-    password = "11111111"
-    admin = True
-    user = User(username=username, email=email, password=password, admin=admin)
-    #print("superadmin created", u)
-    db.session.add(user)
+    click.echo('Hello! Run command ok before')
+    u = User(username="superadmin", email="test@gmail.com", password="12345", admin=True)
+    # print("superadmin created", u)
+    db.session.add(u)
     db.session.commit()
-    click.echo("admin created!!!")
+    click.echo("superadmin created!!!")
+
 
 cli.add_command(create_tables)
 cli.add_command(create_admin)
 
-
 if __name__ == '__main__':
     cli()
-# @click.command(name="create_user")
-# @with_appcontext
-# def create_user():
-#     user = User(username='username', email='email', password='password')
-#     db.session.add(user)
-#     db.session.commit()
